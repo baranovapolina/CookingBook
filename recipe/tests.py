24a16f78a54773020a95b7e4ae6c32ac4188f0ed
Polina Baranova <emailforeducation1@gmail.com>
2025-05-21 17:10:10 +0300

    added tests for project

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Recipe, RecipeCategory
 
-# Create your tests here.
+class ViewsTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.category = RecipeCategory.objects.create(name="Desserts")
+
+        # Створюємо 15 рецептів
+        for i in range(15):
+            Recipe.objects.create(
+                title=f"Recipe {i}",
+                description="Опис рецепта",
+                category=self.category
+            )
+
+    def test_main_view_returns_10_or_less(self):
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main.html')
+        self.assertLessEqual(len(response.context['recipes']), 10)
+
+    def test_category_detail_view(self):
+        response = self.client.get(reverse('category_detail', args=[self.category.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'category_detail.html')
+        self.assertEqual(response.context['category'], self.category)
+        self.assertEqual(len(response.context['recipes']), 15)
